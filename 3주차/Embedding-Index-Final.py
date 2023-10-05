ac5162dfc7c8d4ef2222f0cf035ce8c5ddae41b7
안정진 <120768669+An-Jeong-Jin@users.noreply.github.com>
2023-10-06 02:43:24 +0900

    Update Embedding-Index-Final.py

--- 3주차/Embedding-Index-Final.py
+++ 3주차/Embedding-Index-Final.py
@@ -1,51 +1,57 @@
-########3주차 크롤링 코드 완성
-
 import requests
 from bs4 import BeautifulSoup
 import re
 from google.colab import drive
-url = 'https://www.82cook.com/entiz/enti.php?bn=10'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-}
-res = requests.get(url, headers=headers)
-soup = BeautifulSoup(res.text, 'lxml')
-a = []
-link_list = []
 
-t = soup.select('#bbs > ul > li > div > a')
-for i in t:
-    a.append(i.get('href'))
+num_pages = 3
 
-for text in a:
-    link = "https://www.82cook.com/entiz/" + text
-    link_list.append(link)
-l = []
-t = []
-for link in link_list:
-    res = requests.get(link, headers=headers)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
+
+combine_list = []
+
+for page_num in range(1, num_pages + 1):
+    url = f'https://www.82cook.com/entiz/enti.php?bn=10&page={page_num}'
+    res = requests.get(url, headers=headers)
     soup = BeautifulSoup(res.text, 'lxml')
 
-    content = soup.select('#column2')
-    for i in content:
-        l.append(i.select_one('#readTitle > h2').text.strip().replace(" ", "").replace("※", "").replace("!", ""))    ###전처리도 동시에
-        t.append(i.select_one('#articleBody').text.strip().replace(" ", "").replace("※", "").replace("!", ""))
-combine_list = []
-for text, title in zip(t, l):
-    title = ''.join(title.split()) ####들여쓰기 공백 지우기
-    text = ''.join(text.split())
-    combine_text = f"메뉴: {title} \n 레시피:{text}"
-    combine_list.append(combine_text)
-print(combine_list[3])
+    a = []
+    link_list = []
 
-####드라이브 마운트
-from google.colab import drive
+    t = soup.select('#bbs > ul > li > div > a')
+    for i in t:
+        a.append(i.get('href'))
+
+    for text in a:
+        link = "https://www.82cook.com/entiz/" + text
+        link_list.append(link)
+    l = []
+    t = []
+    for link in link_list:
+        res = requests.get(link, headers=headers)
+        soup = BeautifulSoup(res.text, 'lxml')
+
+        content = soup.select('#column2')
+        for i in content:
+            l.append(i.select_one('#readTitle > h2').text.strip().replace(" ", "").replace("※", "").replace("!", ""))
+            t.append(i.select_one('#articleBody').text.strip().replace(" ", "").replace("※", "").replace("!", ""))
+
+    for text, title in zip(t, l):
+        title = ''.join(title.split())
+        text = re.sub(r'[^가-힣ㄱ-ㅎㅏ-ㅣ ]', '', text)  ##### 한글을 제외한 모든 문자 제거
+        combine_text = f"메뉴: {title} \n 레시피:{text}"
+        combine_list.append(combine_text)
+
+#####크롤링한 레시피들 출력
+for i in combine_list:
+  print(f"Hit recipe \n {i}\n")
+#####드라이브 마운트
 drive.mount('/content/drive')
-#### 해당경로로 저장
-for index, combine_text in enumerate(combine_list):
-    file_name = f'/content/drive/MyDrive/Colab Notebook/recipe/레시피_{index+1}.txt'
+##### 해당경로로 저장
+for idx, combine_text in enumerate(combine_list):
+    file_name = f'/content/drive/MyDrive/Colab Notebook/recipe/레시피_{idx + 1}.txt'
     with open(file_name, 'w', encoding='utf-8') as file:
         file.write(combine_text)
+
 ##################### 크롤링 후 전처리 및 드라이브 마운트를 통한 레시피 텍스트 파일저장 
 #######################임베딩 및 색인 시작
 
@@ -94,7 +100,7 @@ query = "전남친 토스트 레시피 알려줘"
 docs = db.similarity_search(query)
 ######
 
-docs[0]
+docs[:5]
 
 
 

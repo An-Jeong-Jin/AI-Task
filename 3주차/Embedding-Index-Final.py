8e2a0a5df8e23301b888811802c1b54e3ad0082c
안정진 <120768669+An-Jeong-Jin@users.noreply.github.com>
2023-10-07 01:55:38 +0900

    Update Embedding-Index-Final.py

--- 3주차/Embedding-Index-Final.py
+++ 3주차/Embedding-Index-Final.py
@@ -3,43 +3,34 @@ from bs4 import BeautifulSoup
 import re
 from google.colab import drive
 
-num_pages = 3
+num_pages = 2
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
 
 combine_list = []
-l = []
-t = []
-a = []
-link_list = []
 
 for page_num in range(1, num_pages + 1):
-    url = f'https://www.82cook.com/entiz/enti.php?bn=10&page={page_num}'
-    res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.text, 'lxml')
+  a = []
+  url = f'https://www.82cook.com/entiz/enti.php?bn=10&page={page_num}'
+  res = requests.get(url, headers=headers)
+  soup = BeautifulSoup(res.text, 'lxml')
+  t = soup.select('#bbs > ul > li > div > a')
+  for i in t:
+    a.append(i.get('href'))
 
-    t = soup.select('#bbs > ul > li > div > a')
-    for i in t:
-        a.append(i.get('href'))
-
-    for text in a:
-        link = "https://www.82cook.com/entiz/" + text   #####다른 사이트와 다르게 href="read.php?bn=10&num=3245201&page=0"와 같이 뒷부분만 가져올 수 있어 해당 링크에 붙여줌
-        link_list.append(link)
-
-for link in link_list:
+  for text in a:
+    link = "https://www.82cook.com/entiz/" + text        #####다른 사이트와 다르게 href="read.php?bn=10&num=3245201&page=0"와 같이 뒷부분만 가져올 수 있어 해당 링크에 붙여줌
     res = requests.get(link, headers=headers)
     soup = BeautifulSoup(res.text, 'lxml')
 
-    content = soup.select('#column2')               ###텍스트 전체를 박스 찾기
+    content = soup.select('#column2')
     for i in content:
-        l.append(i.select_one('#readTitle > h2').text.strip().replace(" ", "").replace("※", "").replace("!", ""))     ######전체를 포함하는 박스에서 레시피 이름만 가져오기
-        t.append(i.select_one('#articleBody').text.strip().replace(" ", "").replace("※", "").replace("!", ""))        ######전체를 포함하는 박스에서 레시피 내용만 가져오기
-
-for text, title in zip(t, l):
-    title = ''.join(title.split())
-    text = re.sub(r'[^가-힣ㄱ-ㅎㅏ-ㅣ ]', '', text)   ##### 한글을 제외한 모든 문자 제거
-    combine_text = f"메뉴: {title} \n 레시피:{text}"
-    combine_list.append(combine_text)
+      title = i.select_one('#readTitle > h2').text.strip().replace(" ", "").replace("※", "").replace("!", "")      ######전체를 포함하는 박스에서 레시피 이름만 가져오기
+      text = i.select_one('#articleBody').text.strip().replace(" ", "").replace("※", "").replace("!", "")          ######전체를 포함하는 박스에서 레시피 내용만 가져오기
+      text = re.sub(r'[^가-힣ㄱ-ㅎㅏ-ㅣ ]', '', text)          ##### 한글을 제외한 모든 문자 제거
+      title = re.sub(r'[^가-힣ㄱ-ㅎㅏ-ㅣ ]', '', title)        ##### 한글을 제외한 모든 문자 제거
+      combine_text = f"메뉴: {title}\n레시피: {text}"
+      combine_list.append(combine_text)
 
 #####크롤링한 레시피들 출력
 for i in combine_list:

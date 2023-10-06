c87cb5b690f7d4b7e1f9f94cefa7e8a566bc05e1
안정진 <120768669+An-Jeong-Jin@users.noreply.github.com>
2023-10-06 23:39:38 +0900

    Update Embedding-Index-Final.py

--- 3주차/Embedding-Index-Final.py
+++ 3주차/Embedding-Index-Final.py
@@ -8,14 +8,15 @@ num_pages = 3
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
 
 combine_list = []
+l = []
+t = []
+a = []
+link_list = []
 
 for page_num in range(1, num_pages + 1):
     url = f'https://www.82cook.com/entiz/enti.php?bn=10&page={page_num}'
     res = requests.get(url, headers=headers)
     soup = BeautifulSoup(res.text, 'lxml')
-
-    a = []
-    link_list = []
 
     t = soup.select('#bbs > ul > li > div > a')
     for i in t:
@@ -24,22 +25,27 @@ for page_num in range(1, num_pages + 1):
     for text in a:
         link = "https://www.82cook.com/entiz/" + text
         link_list.append(link)
-    l = []
-    t = []
-    for link in link_list:
-        res = requests.get(link, headers=headers)
-        soup = BeautifulSoup(res.text, 'lxml')
 
-        content = soup.select('#column2')
-        for i in content:
-            l.append(i.select_one('#readTitle > h2').text.strip().replace(" ", "").replace("※", "").replace("!", ""))
-            t.append(i.select_one('#articleBody').text.strip().replace(" ", "").replace("※", "").replace("!", ""))
+l = []
+t = []
 
-    for text, title in zip(t, l):
-        title = ''.join(title.split())
-        text = re.sub(r'[^가-힣ㄱ-ㅎㅏ-ㅣ ]', '', text)  ##### 한글을 제외한 모든 문자 제거
-        combine_text = f"메뉴: {title} \n 레시피:{text}"
-        combine_list.append(combine_text)
+for link in link_list:
+    res = requests.get(link, headers=headers)
+    soup = BeautifulSoup(res.text, 'lxml')
+
+    content = soup.select('#column2')
+    for i in content:
+        l.append(i.select_one('#readTitle > h2').text.strip().replace(" ", "").replace("※", "").replace("!", ""))
+        t.append(i.select_one('#articleBody').text.strip().replace(" ", "").replace("※", "").replace("!", ""))
+
+for text, title in zip(t, l):
+    title = ''.join(title.split())
+    text = re.sub(r'[^가-힣ㄱ-ㅎㅏ-ㅣ ]', '', text)   ##### 한글을 제외한 모든 문자 제거
+    combine_text = f"메뉴: {title} \n 레시피:{text}"
+    combine_list.append(combine_text)
+
+for i in combine_list:
+    print(f"Hit recipe \n {i}\n")
 
 #####크롤링한 레시피들 출력
 for i in combine_list:

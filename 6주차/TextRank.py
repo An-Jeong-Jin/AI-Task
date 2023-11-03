ed6e8dc32dbabd7bb772e1885cbfd4ff44b5255e
안정진 <120768669+An-Jeong-Jin@users.noreply.github.com>
2023-11-03 23:46:58 +0900

    Update TextRank.py

--- 6주차/TextRank.py
+++ 6주차/TextRank.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from gensim.summarization import keywords, summarize
 import re
 
-num_pages = 3
+num_pages = 1
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 }
@@ -27,9 +27,8 @@ for page_num in range(1, num_pages + 1):
 
         content = soup.select('#column2')
         for i in content:
-            title = i.select_one('#readTitle > h2').text.strip()
-            text = i.select_one('#articleBody').text.strip()
-
+            title = i.select_one('#readTitle > h2').text.strip().replace(".", "/").replace("※", "")
+            text = i.select_one('#articleBody').text.strip().replace(".", "/").replace("※", "")
             recipe_keywords = keywords(text, words=5, split=True, scores=True, lemmatize=True)    ### TextRank 알고리즘을 사용하여 레시피 추출적 요약
             recipe_summarize = summarize(text, word_count=50)
 
@@ -37,5 +36,6 @@ for page_num in range(1, num_pages + 1):
             summarize_recipe = f"레시피: {title}\n키워드: {recipe_keywords}\n요약된 레시피: {recipe_summarize}"  ### 레시피 제목과 내용을 결합하여 저장
             combine_list.append(summarize_recipe)
 
+
 for index, summary in enumerate(combine_list):    ### 요약된 레시피 출력
-    print(f"레시피{index + 1} 번 요약:\n{summary}\n")
+    print(f"레시피 {index + 1}번 요약:\n{summary}\n")

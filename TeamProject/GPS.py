4d84dffb8e6f510b71ba6cf4543fa5ad94acf9f2
안정진 <120768669+An-Jeong-Jin@users.noreply.github.com>
2023-12-15 12:35:29 +0900

    Update GPS.py

--- TeamProject/GPS.py
+++ TeamProject/GPS.py
@@ -2,7 +2,7 @@ import requests
 import googlemaps
 import json
 
-GOOGLE_API_KEY = "AIzaSyD1m-aX-rG5rf4DbIVBZCNYrnw9xRXa9YY"
+GOOGLE_API_KEY = "AIzaSyD1m-aX-rG5rf4DbIVBZCNYrnw9xRXa9YY"       ## api키 예시
 url = f'https://www.googleapis.com/geolocation/v1/geolocate?key={GOOGLE_API_KEY}'
 data = {
     'considerIp': True, # 현 IP로 데이터 추출
